48e961a9976cfd1fbe97c020dbed6423bf8e2577
BeansTown106 <nbenis106@gmail.com>
2018-08-28 13:14:16 +0800

    add activity to launch headsup settings fragment

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -137,6 +137,7 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import com.wave.settings.fragments.HeadsUpSettings;
 import com.wave.settings.WaveSettings;
 
 public class SettingsGateway {
@@ -263,7 +264,8 @@ public class SettingsGateway {
             DirectoryAccessDetails.class.getName(),
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
-            WaveSettings.class.getName()
+            WaveSettings.class.getName(),
+            HeadsUpSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
