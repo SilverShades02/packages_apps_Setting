9bd0aa00928fc5fd26503700200cd08dc660189e
xyyx <xyyx@mail.ru>
2018-08-28 13:15:30 +0800

    Export ButtonSettings for tile

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -166,5 +166,6 @@ public class Settings extends SettingsActivity {
     public static class SystemDashboardActivity extends SettingsActivity {}
     public static class WaveSettingsActivity extends SettingsActivity {}
     public static class HeadsUpSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class ButtonSettingsActivity extends SettingsActivity { /* empty */ }
 
 }
